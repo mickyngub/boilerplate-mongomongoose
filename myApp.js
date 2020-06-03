8f922f2fc223cb96ae64888e55d4ee193daf8e15
mickyngub <micky47561@gmail.com>
2020-06-03 08:56:34 +0000

    #12 .find().sort().limit().select().exec()

--- myApp.js
+++ myApp.js
@@ -327,8 +327,14 @@ var removeManyPeople = function(done) {
 
 var queryChain = function(done) {
   var foodToSearch = "burrito";
-  
-  done(null/*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age:0}).exec((err,data) => {
+    if(err) {
+      console.error(err);
+    }
+    console.log("succesfully chained");
+    done(null,data);
+  });
+
 };
 
 /** **Well Done !!**
